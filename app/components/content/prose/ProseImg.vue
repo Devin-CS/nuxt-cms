5a4185fff3063c8530f51c3bb657a4dccb88dacd
devin <devin.holloway@coopersurgical.com>
2025-09-15 10:07:06 -0600

    Add `ProseImg` component for handling responsive images with customizable dimensions

---
+++ app/components/content/prose/ProseImg.vue
@@ -0,0 +1,28 @@
+<template>
+<q-img
+  :src="props.src"
+  :alt="props.alt"
+  :width="widthStyle"
+  :height="heightStyle"
+  v-bind="$attrs"/>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  src?: string
+  alt?: string
+  width?: string | number
+  height?: string | number
+}>(), {
+  src: '',
+  alt: ''
+})
+
+const widthStyle = computed(() => props.width !== undefined
+  ? (typeof props.width === 'number' ? `${props.width}px` : props.width)
+  : undefined)
+
+const heightStyle = computed(() => props.height !== undefined
+  ? (typeof props.height === 'number' ? `${props.height}px` : props.height)
+  : undefined)
+</script>
