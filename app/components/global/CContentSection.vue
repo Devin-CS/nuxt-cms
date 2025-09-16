e7626f29a4f2583b5889d949cbb1a3e90456403e
devin <devin.holloway@coopersurgical.com>
2025-09-16 10:56:48 -0600

    Add `CContentSection` component and integrate it into the homepage for new call-to-action section.

---
+++ app/components/global/CContentSection.vue
@@ -0,0 +1,38 @@
+<template>
+<section>
+  <q-card>
+    <q-card-section>
+      <h2 class="text-h2">
+        {{ title }}
+      </h2>
+      <p
+        v-if="description"
+        class="text-subtitle2">
+        {{ description }}
+      </p>
+    </q-card-section>
+    <q-card-actions align="center">
+      <slot name="buttons"/>
+    </q-card-actions>
+  </q-card>
+
+  <slot name="content"/>
+</section>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  /** CTA title text */
+  title: string
+  /** CTA description text */
+  description?: string
+  /** Background color using brand palette or transparent */
+  background?: | 'white' | 'black' | 'dark' | 'transparent' | 'primary' | 'secondary' | 'accent' | 'positive' | 'negative' | 'info' | 'warning'
+}>(), {
+  background: 'transparent'
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
