83414fc2bbcdaffd47965a1b08f48dfb15135dc7
devin <devin.holloway@coopersurgical.com>
2025-11-15 18:33:10 -0700

    Refactor: Improve `CTooltip` styling with updated classes and attributes for better appearance and shadow effect

--- app/components/global/CTooltip.vue
+++ app/components/global/CTooltip.vue
@@ -1,8 +1,10 @@
 <template>
-<span class="cursor-pointer" tabindex="0">
+<span
+  class="cursor-pointer"
+  tabindex="0">
   <slot/>
   <q-tooltip
-    class="bg-shell text-pine text-body1"
+    class="bg-white text-pine text-body1 shadow-4"
     max-width="300px"
     transition-show="scale"
     transition-hide="scale">
